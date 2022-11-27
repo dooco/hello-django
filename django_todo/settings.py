3c589edf986fc10d38fe35b314e1bb5ab639bf1a
dooco <padraigdooley@gmail.com>
2022-11-27 21:34:38 +0000

    add allowed hosts to settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,8 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dooco-django-todo-app.herokuapp.com']
 
 
 # Application definition
