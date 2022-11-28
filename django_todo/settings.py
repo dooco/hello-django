db531f5b2d427fd72ce88d276a7b1478fed9329a
dooco <padraigdooley@gmail.com>
2022-11-28 20:43:39 +0000

    hide sectret_key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 ALLOWED_HOSTS = ['dooco-django-todo-app.herokuapp.com']
